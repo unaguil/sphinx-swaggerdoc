1bc55e8bda5b156a123c8f01d222a8995bed40d7
Unai Aguilera <unai.aguilera@deusto.es>
2015-02-06 14:34:06 +0100

    Incorrect import solved

--- sphinx_swagger/__init__.py
+++ sphinx_swagger/__init__.py
@@ -1,7 +1,7 @@
-from swagger_doc import swagger_doc, visit_swaggerdoc_node, depart_swaggerdoc_node, SwaggerDocDirective
+from swagger_doc import swaggerdoc, visit_swaggerdoc_node, depart_swaggerdoc_node, SwaggerDocDirective
 
 def setup(app):
-    app.add_node(swagger_doc,
+    app.add_node(swaggerdoc,
                  html=(visit_swaggerdoc_node, depart_swaggerdoc_node),
                  latex=(visit_swaggerdoc_node, depart_swaggerdoc_node),
                  text=(visit_swaggerdoc_node, depart_swaggerdoc_node))
