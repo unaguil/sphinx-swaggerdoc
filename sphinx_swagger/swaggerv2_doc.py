c81ae880c2793f3556041d9ed35d78f294197507
Unai Aguilera <unai.aguilera@deusto.es>
2016-09-19 16:20:46 +0200

    Methods grouped by tag

--- sphinx_swagger/swaggerv2_doc.py
+++ sphinx_swagger/swaggerv2_doc.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from docutils import nodes
+import traceback
 
 from docutils.parsers.rst import Directive
 
@@ -60,22 +61,50 @@ class SwaggerV2DocDirective(Directive):
 
         return [swagger_node]
 
+    def group_tags(self, api_desc):
+        groups = {}
+
+        for tag in api_desc['tags']:
+            groups[tag['name']] = []
+
+        for path, methods in api_desc['paths'].items():
+            for method_type, method in methods.items():
+                for tag in method['tags']:
+                    groups[tag].append((path, method_type, method))
+
+        return groups
+
+    def create_section(self, title):
+        section = nodes.section(ids=[title])
+        section += nodes.title(title, title)
+        return section
+
     def run(self):
         try:
             api_desc = self.processSwaggerURL(self.content[0])
 
+            groups = self.group_tags(api_desc)
+
             entries = []
-            for path, methods in api_desc['paths'].items():
-                for method_type, method in methods.items():
-                    entries += self.make_method(path, method_type, method)
+            for tag_name, methods in groups.items():
+                section = self.create_section(tag_name)
+
+                for path, method_type, method in methods:
+                    section += self.make_method(path, method_type, method)
+
+                entries.append(section)
 
             return entries
-        except:
-            print('Unable to process URL: %s' % self.content[0])
+        except Exception as e:
+            error_message = 'Unable to process URL: %s' % self.content[0]
+            print error_message
+            traceback.print_exc()
+
             error = nodes.error('')
-            para = nodes.paragraph()
-            para += nodes.Text('Unable to process URL: ')
-            para += nodes.strong('', self.content[0])
-            para += nodes.Text('. Please check that the URL is a valid Swagger api-docs URL and it is accesible')
-            error += para
+            para_error = nodes.paragraph()
+            para_error += nodes.Text(error_message + '. Please check that the URL is a valid Swagger api-docs URL and it is accesible')
+            para_error_detailed = nodes.paragraph()
+            para_error_detailed = nodes.strong('Processing error. See console output for a more detailed error')
+            error += para_error
+            error += para_error_detailed
             return [error]
